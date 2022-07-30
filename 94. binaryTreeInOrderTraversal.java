e71c6b556d8f05f29f9c4af013f9a17727c2c64a
Justin Etu <71947079+justinetu@users.noreply.github.com>
2022-07-30 17:11:23 -0500

    Create 94. binaryTreeInOrderTraversal.java

---
+++ 94. binaryTreeInOrderTraversal.java
@@ -0,0 +1,28 @@
+/*
+  Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+  Example 1:
+
+  Input: root = [1,null,2,3]
+  Output: [1,3,2]
+*/
+
+class binaryTreeInOrderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> myList = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        if(root == null)
+            return myList;
+        while(curr != null || !stack.isEmpty()) {
+            while(curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            myList.add(curr.val);
+            curr = curr.right;
+        }
+        return myList;
+    }
+}
