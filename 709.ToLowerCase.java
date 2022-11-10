012b3663452d4be3d9cce735a1285c4cca9f5bb2
Justin Etu <71947079+justinetu@users.noreply.github.com>
2022-11-09 22:40:43 -0600

    Create 709.ToLowerCase.java

---
+++ 709.ToLowerCase.java
@@ -0,0 +1,17 @@
+/*
+  Question
+  Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+  
+  Example 1:
+
+  Input: s = "Hello"
+  Output: "hello"
+*/
+
+// Answer
+
+class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
